a62bdd29626cfad065d44954171d31ffaa60fb1c
mahdi <mahdibiology100@gmail.com>
2025-09-24 21:48:28 +0330

    adding a mock file for better understanding about API concept

---
+++ src/mock/browsCategory.ts
@@ -0,0 +1,62 @@
+export const browsCategoryMock = [
+    {
+        icon: "icon-groceries-1",
+        iconPath: 7,
+        title: "Milks and Dairies",
+        link: "#",
+    },
+    {
+        icon: "icon-drinks-1",
+        iconPath: 5,
+        title: "Wines & Drinks",
+        link: "#",
+    },
+    {
+        icon: "icon-dress-1",
+        iconPath: 4,
+        title: "Clothing & Beauty",
+        link: "#",
+    },
+    {
+        icon: "icon-crab-1",
+        iconPath: 9,
+        title: "Fresh Seafood",
+        link: "#",
+    },
+    {
+        icon: "icon-pet-food-1",
+        iconPath: 8,
+        title: "Pet Foods & Toys",
+        link: "#",
+    },
+    {
+        icon: "icon-sweet-1",
+        iconPath: 7,
+        title: "Fast Food",
+        link: "#",
+    },
+    {
+        icon: "icon-dairy-products-1",
+        iconPath: 5,
+        title: "Baking Material",
+        link: "#",
+    },
+    {
+        icon: "icon-grocery-1",
+        iconPath: 7,
+        title: "Vegetables",
+        link: "#",
+    },
+    {
+        icon: "icon-fruit-1",
+        iconPath: 3,
+        title: "Fresh Fruit",
+        link: "#",
+    },
+    {
+        icon: "icon-milk-products-1",
+        iconPath: 5,
+        title: "Bread and Juice",
+        link: "#",
+    }
+]
