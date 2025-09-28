a0533ecd0a33304bc384fc896ddc3525e67b636b
mahdi <mahdibiology100@gmail.com>
2025-09-28 11:42:49 +0330

    debugging featured category & bestSellers Section, some changes in appearance.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,9 @@ export default function Home() {
               <div className="hidden sm:flex mb-[50px]">
                   <h2 className="text-heading3 text-blue-300">Featured Categories</h2>
               </div>
-              <FeaturedCategories/>
+              <div className={"flex"}>
+                <FeaturedCategories/>
+              </div>
           </Section>
           <Section>
               <MiniProductSlider/>
@@ -60,18 +62,20 @@ export default function Home() {
                   <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Best
                       Sellers</h2>
               </div>
-              <div className="flex gap-[24px]">
-                  <div
-                      className="bg-[url('/assets/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-12 pt-[38px] self-stretch flex-col justify-between max-w-[370px] hidden xl:flex">
-                      <h3 className="text-heading2 text-blue-300">Bring nature into your home</h3>
-                      <Link href="#"
-                         className="mt-6 pl-[15px] pr-2.5 py-2 bg-yellow-100 hover:bg-green-200 rounded-[3px] cursor-pointer inline-flex max-w-max items-center gap-2.5">
-                          <div className="text-xsmall text-white">Shop now</div>
-                          <IconBox icon={"icon-arrow-small-right"} size={24}/>
-                      </Link>
+              <div className={"grid grid-cols-4 gap-4"}>
+                  <div className="flex gap-[24px]">
+                      <div
+                          className="bg-[url('/assets/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-12 pt-[38px] self-stretch flex-col justify-between max-w-[370px] hidden xl:flex">
+                          <h3 className="text-heading2 text-blue-300">Bring nature into your home</h3>
+                          <Link href="#"
+                                className="mt-6 pl-[15px] pr-2.5 py-2 bg-yellow-100 hover:bg-green-200 rounded-[3px] cursor-pointer inline-flex max-w-max items-center gap-2.5">
+                              <div className="text-xsmall text-white">Shop now</div>
+                              <IconBox icon={"icon-arrow-small-right"} size={24}/>
+                          </Link>
+                      </div>
                   </div>
+                  <div className={"col-span-3"}><BestSellersSlider sliderData={BestSellers}/></div>
               </div>
-              <BestSellersSlider sliderData={BestSellers}/>
           </Section>
           <Section>
               <div className="flex justify-between items-center mb-[50px]">
