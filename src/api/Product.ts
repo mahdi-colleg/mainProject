8cc575ddcca94e185839641677bfc738abb2720d
mahdi <mahdibiology100@gmail.com>
2025-10-11 15:50:18 +0330

    implementing api for best sellers slider

--- src/api/Product.ts
+++ src/api/Product.ts
@@ -7,24 +7,26 @@ interface Props{
     filters?: {
         is_popular?: boolean;
         is_popular_fruit?: boolean;
+        is_best_seller?: boolean;
     }
 }
 
 interface Filters{
     is_popular?: {$eq: boolean}
     is_popular_fruit?: {$eq: boolean}
+    is_best_seller?: {$eq: boolean}
 }
 
 export function getAllProducts({populate, filters}:Props):Promise<ApiResponseType<ProductType>> {
 
     const customFilters : Filters = {};
 
-    if (filters?.is_popular) {
-        customFilters.is_popular = {$eq: filters.is_popular}
-    }
-    if (filters?.is_popular_fruit) {
-        customFilters.is_popular_fruit = {$eq: filters.is_popular_fruit}
-    }
+    filters?.is_popular && (customFilters.is_popular = {$eq: filters.is_popular})
+
+    filters?.is_popular_fruit && (customFilters.is_popular_fruit = {$eq: filters.is_popular_fruit})
+
+    filters?.is_best_seller && (customFilters.is_best_seller = {$eq: filters.is_best_seller})
+
 
     return apiClient.get("/products", {
         params:{
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Section} from "@/components/layouts/section";
 import {
-    Banner,
+    Banner, BestSellersSlider,
     FeaturedCategories, GeneralProductSlider,
     IconBox,
     MiniProductSlider,
@@ -11,6 +11,7 @@ import {getAllProducts} from "@/api/Product";
 import {useQuery} from "@tanstack/react-query";
 import {ApiResponseType} from "@/types";
 import {ProductType} from "@/types/api/Product";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -23,6 +24,11 @@ export default function Home() {
     const {data: popularFruitProductsData} = useQuery<ApiResponseType<ProductType>>({
         queryKey:[getAllProducts.name, "popular-fruit"],
         queryFn: ()=>getAllProducts({populate:["categories", "thumbnail"], filters:{is_popular_fruit:true}})
+    })
+
+    const {data: bestSellerProductsData} = useQuery<ApiResponseType<ProductType>>({
+        queryKey:[getAllProducts.name, "best-seller"],
+        queryFn: ()=>getAllProducts({populate:["categories", "thumbnail"], filters:{is_best_seller:true}})
     })
 
 
@@ -66,26 +72,26 @@ export default function Home() {
               </div>
               {popularFruitProductsData && <SimpleProductSlider sliderData={popularFruitProductsData.data} nextEl={".swiper-nav-right2"} prevEl={".swiper-nav-left2"} />}
           </Section>
-          {/*<Section>*/}
-          {/*    <div className="flex justify-between mb-[50px]">*/}
-          {/*        <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Best*/}
-          {/*            Sellers</h2>*/}
-          {/*    </div>*/}
-          {/*    <div className={"inline-grid grid-cols-4 gap-4"}>*/}
-          {/*        <div className="flex gap-[24px]">*/}
-          {/*            <div*/}
-          {/*                className="bg-[url('/assets/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-12 pt-[38px] self-stretch flex-col justify-between max-w-[370px] hidden xl:flex">*/}
-          {/*                <h3 className="text-heading2 text-blue-300">Bring nature into your home</h3>*/}
-          {/*                <Link href="#"*/}
-          {/*                      className="mt-6 pl-[15px] pr-2.5 py-2 bg-yellow-100 hover:bg-green-200 rounded-[3px] cursor-pointer inline-flex max-w-max items-center gap-2.5">*/}
-          {/*                    <div className="text-xsmall text-white">Shop now</div>*/}
-          {/*                    <IconBox icon={"icon-arrow-small-right"} size={24}/>*/}
-          {/*                </Link>*/}
-          {/*            </div>*/}
-          {/*        </div>*/}
-          {/*        <div className={"col-span-4 xl:col-span-3"}><BestSellersSlider sliderData={BestSellers}/></div>*/}
-          {/*    </div>*/}
-          {/*</Section>*/}
+          <Section>
+              <div className="flex justify-between mb-[50px]">
+                  <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Best
+                      Sellers</h2>
+              </div>
+              <div className={"inline-grid grid-cols-4 gap-4"}>
+                  <div className="flex gap-[24px]">
+                      <div
+                          className="bg-[url('/assets/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-12 pt-[38px] self-stretch flex-col justify-between max-w-[370px] hidden xl:flex">
+                          <h3 className="text-heading2 text-blue-300">Bring nature into your home</h3>
+                          <Link href="#"
+                                className="mt-6 pl-[15px] pr-2.5 py-2 bg-yellow-100 hover:bg-green-200 rounded-[3px] cursor-pointer inline-flex max-w-max items-center gap-2.5">
+                              <div className="text-xsmall text-white">Shop now</div>
+                              <IconBox icon={"icon-arrow-small-right"} size={24}/>
+                          </Link>
+                      </div>
+                  </div>
+                  {bestSellerProductsData && <div className={"col-span-4 xl:col-span-3"}><BestSellersSlider sliderData={bestSellerProductsData.data}/></div>}
+              </div>
+          </Section>
           {/*<Section>*/}
           {/*    <div className="flex justify-between items-center mb-[50px]">*/}
           {/*        <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Deals*/}
